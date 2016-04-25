9dfb1601b8e3df300a5775ad1d23e96834d4c26d
ErikAbrahamson <e.a.abrahamson@gmail.com>
2016-04-25 13:54:48 -0600

    update line spacing

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 (function(ns) {
 
     ns.graphSort = function(w) {
-
         w.on('beforeviewloaded', function(w, args) {
-
             var series = args.options.series,
                 categories = [], axis;
 
@@ -18,46 +16,39 @@
         		});
             }
 
-        	categories.sort(function(a, b) {
-        		var aName = a.name.toLowerCase(),
-        			bName = b.name.toLowerCase(),
-        			aTotal = a.stackTotal,
-        			bTotal = b.stackTotal;
+            categories.sort(function(a, b) {
+                var aName = a.name.toLowerCase(),
+                    bName = b.name.toLowerCase(),
+                    aTotal = a.stackTotal,
+                    bTotal = b.stackTotal;
 
-        		if (aTotal === bTotal) {
-        			return ((aName < bName) ? -1 : ((aName > bName) ? 1 : 0));
-        		} else {
-        			return ((aTotal > bTotal) ? -1 : ((aTotal < bTotal) ? 1 : 0));
-        		}
-        	});
+                if (aTotal === bTotal) {
+                    return ((aName < bName) ? -1 : ((aName > bName) ? 1 : 0));
+                } else {
+                    return ((aTotal > bTotal) ? -1 : ((aTotal < bTotal) ? 1 : 0));
+                }
+            });
 
-        	var mappedIndex = _.map(categories, function(category, index) {
-        		return category.index;
-        	});
+            var mappedIndex = _.map(categories, function(category, index) {
+                return category.index;
+            });
+            categories = _.map(categories, function(category, index) {
+                return category.name;
+            });
+            setTimeout(function() {
+                axis = Highcharts.charts[0].xAxis[0];
+                axis.setCategories(categories);
+            }, 100);
 
-        	categories = _.map(categories, function(category, index) {
-        		return category.name;
-        	});
-
-        	setTimeout(function() {
-        		axis = Highcharts.charts[0].xAxis[0];
-        		axis.setCategories(categories);
-        	}, 100);
-
-
-        	_.each(series, function(node) {
-
-        		var data = _.map(mappedIndex, function(mappedIndex, origIndex) {
-        			return node.data[mappedIndex].y;
-        		});
-
-        		setTimeout(function() {
-        			series = Highcharts.charts[0].series[0];
-        			series.setData(data);
-        		});
-
-        	});
-
+            _.each(series, function(node) {
+                var data = _.map(mappedIndex, function(mappedIndex, origIndex) {
+                    return node.data[mappedIndex].y;
+                });
+                setTimeout(function() {
+                    series = Highcharts.charts[0].series[0];
+                    series.setData(data);
+                });
+            });
         });
     };
 })(window);
